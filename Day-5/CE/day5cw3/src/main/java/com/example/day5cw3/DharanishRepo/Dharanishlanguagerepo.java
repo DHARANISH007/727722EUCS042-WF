9ca22309b815e17b90261bbfc4ad021ebd64cf7d
DHARANISH007 <124783248+DHARANISH007@users.noreply.github.com>
2024-04-24 00:33:37 +0530

    Delete Day-5/CE directory

--- Day-5/CE/day5cw3/src/main/java/com/example/day5cw3/DharanishRepo/Dharanishlanguagerepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day5cw3.DharanishRepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day5cw3.DharanishModel.DharanishLanguage;
-
-public interface Dharanishlanguagerepo extends JpaRepository<DharanishLanguage,Integer>{
-    
-}
