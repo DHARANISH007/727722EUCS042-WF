46b1b085720df6417986c9ed673c79f42712b67c
DHARANISH007 <124783248+DHARANISH007@users.noreply.github.com>
2024-04-24 00:34:22 +0530

    Delete Day-6 directory

--- Day-6/cy/day6cy/src/main/java/com/example/day6cy/repository/DharanishEmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day6cy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day6cy.model.DharanishEmployee;
-
-public interface DharanishEmployeeRepo extends JpaRepository<DharanishEmployee,Integer>{
-    
-}
