0ea8aa66af0fc76d71d520470d03297986944c13
DHARANISH007 <124783248+DHARANISH007@users.noreply.github.com>
2024-04-29 12:13:41 +0530

    Delete Day 14/Day 14 directory

--- Day 14/Day 14/day14-ce2/src/main/java/com/example/day14ce2/Dharanishconfiguration/DharanishOpenApiConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.day14ce2.Dharanishconfiguration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DharanishOpenApiConfig {
-    
-}
